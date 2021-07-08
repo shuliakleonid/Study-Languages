6208401baaaf98970537e53eb58f6aee74cdf1e3
Shuliak Leonid <l.shulyak@tanto-s.ru>
2021-07-08 10:54:13 +0300

    add route and settings eslint, husky

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./models/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'grammar',
+    loadChildren: () =>
+      import('./models/grammar-test/grammar-test.module').then((m) => m.GrammarTestModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
