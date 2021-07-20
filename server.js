adbc76b9338810b1edda169c9d121f2a26167ea1
Shuliak Leonid <l.shulyak@tanto-s.ru>
2021-07-20 23:23:02 +0300

    fix build prod

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const path = require('path');
 
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<exadel-trainee-app>'));
+app.use(express.static( './dist/<exadel-trainee-app>'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<exadel-trainee-app>/index.html'));
+  res.sendFile('index.html',{ root:'dist/<exadel-trainee-app>/' });
 });
 
 // Start the app by listening on the default Heroku port
