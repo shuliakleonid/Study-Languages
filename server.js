fe43824f10c24471f56037ca0e8129dffd3da2aa
shuliak leonid <zeleny777@gmail.com>
2021-07-21 09:56:58 +0300

    fix build prod

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-
-
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static( './dist/<exadel-trainee-app>'));
+app.use(express.static(__dirname + '/dist/<exadel-trainee-app>'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile('index.html',{ root:'dist/<exadel-trainee-app>/' });
+  res.sendFile(path.join(__dirname+'/dist/<exadel-trainee-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
